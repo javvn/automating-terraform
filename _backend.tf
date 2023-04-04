151a71fd378e997f7c38c6f4c5c5ad32a9d88a1d
javvn <juho0428@naver.com>
2023-04-04 19:16:30 +0900

    stage branch init

--- _backend.tf
+++ _backend.tf
@@ -1,10 +1,10 @@
-#terraform {
-#  backend "remote" {
-#    hostname     = "app.terraform.io"
-#    organization = "jawn"
-#
-#    workspaces {
-#      prefix = "automating-terraform-"
-#    }
-#  }
-#}
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "jawn"
+
+    workspaces {
+      prefix = "automating-terraform-"
+    }
+  }
+}
